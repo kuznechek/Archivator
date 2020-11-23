14f9da70e6773bee9f2fbbc28c6f8015f3702653
AnastasiaWei <61657226+kuznechek@users.noreply.github.com>
2020-11-24 00:37:33 +0300

    Add files via upload

--- headers/archivator.h
+++ headers/archivator.h
@@ -1,10 +1,16 @@
-int rewrite(int file);
+#define SIZE 4;
+
+int rewrite_file(int file);
 void print_dir(char *dir, int depth);
 int pack(char *input);
 int unpack(char *archiv_path, char *folder);
 char* get_filepath(char* file, char* path);
+char* get_newpath(char* dir, char* file);
 int free_mem(char array[],int n);
-int rewrite_info(char *path);
+int rewrite_info(int type, char *path);
+int rewrite_dir(int type, char *path, int count);
 char* to_char(char buf[]);
 char* from_int_to_char(int numb, int mem);
 int from_char_to_int(char str[]);
+int count_objects_in_dir(char* path);
+void rewrite_type(int type);
